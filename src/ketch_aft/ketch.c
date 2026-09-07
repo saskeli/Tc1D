75d2d92ae2cfaadcf9d53366683f09724e106593
Saska Dönges <saska.donges@helsinki.fi>
2026-09-07 12:25:22 +0300

    Explicit fallthrough not conveniently supported by all platforms, so warning for fallthrough will persist on some compilers

--- src/ketch_aft/ketch.c
+++ src/ketch_aft/ketch.c
@@ -377,7 +377,6 @@ void CalcModelLengthsKet(ttPathPtr tTPath, int numTTNodes, double redLength[],
       /* Just convert the kinetic parameter to Cl apfu
          Note that this invalidates kinPar for the rest of the routine */
       kinPar = kinPar * 0.2978;
-      __attribute__((fallthrough));
     case CL_PFU:
       calc = fabs(kinPar - 1.0);
       if (calc <= 0.130)
